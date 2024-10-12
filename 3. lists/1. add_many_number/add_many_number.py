1c3a17834d02ea581910421f45e779cbbf65d420
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 15:27:32 +0500

    PIAIC python learning projects

--- 3. lists/1. add_many_number/add_many_number.py
+++ 3. lists/1. add_many_number/add_many_number.py
@@ -2,14 +2,14 @@
 # Write a function that takes a list of numbers and returns the sum of those numbers.
 
 
-def add_many_number(numbers):
+def add_many_number(numbers)-> int:
     total_num: int = 0
     for number in numbers:
         total_num += number
     return total_num
 
 def main():
-    numbers: list = [1,2,3,4,5,6,7,8,9]
+    numbers: list = [1,2,3,4,5,6,7,8,9,10]
     sum_of_numbers = add_many_number(numbers)
     print(sum_of_numbers)
     
