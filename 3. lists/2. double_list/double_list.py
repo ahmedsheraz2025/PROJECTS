54eb025289d3cedae7a55f61b8e2b3706b46e119
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 15:56:38 +0500

    PIAIC python learning projects

--- 3. lists/2. double_list/double_list.py
+++ 3. lists/2. double_list/double_list.py
@@ -1,1 +1,16 @@
  
+# Write a program that doubles each element in a list of numbers.
+
+
+def main():
+    numbers: list[int] = [1,2,3,4]
+    
+    for i in range(len(numbers)):
+        j = numbers[i]
+        
+        # Modified numbers[i] :
+        numbers[i] = j*2
+    print(numbers)
+    
+if __name__ == '__main__':
+    main()
