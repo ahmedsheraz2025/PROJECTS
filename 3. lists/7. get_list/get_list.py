80136996af9d5a2ef510e420dae4267e206631d2
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 18:15:47 +0500

    PIAIC python learning projects

--- 3. lists/7. get_list/get_list.py
+++ 3. lists/7. get_list/get_list.py
@@ -1,1 +1,15 @@
  
+# Write a program which continuously asks the user to enter values which
+# are added one by one into a list.When the user presses enter without typing
+# anything, print the list.
+
+def main():
+    lst = []
+    val = input("Enter a value: ")
+    while val:
+        lst.append(val)
+        val = input("Enter a value: ")
+    print(lst)
+    
+if __name__ == '__main__':
+    main()
