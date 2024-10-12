e4281d5ba65ae4918d0ebfe271d68584482ee06a
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 16:48:29 +0500

    PIAIC python learning projects

--- 3. lists/4. flowing_with_data_structures/flowing_with_data_structures.py
+++ 3. lists/4. flowing_with_data_structures/flowing_with_data_structures.py
@@ -1,1 +1,20 @@
- 
+
+def add_three_copies(my_list,data):
+    for i in range(3):
+        my_list.append(data)
+    
+    
+
+
+
+
+def main():
+    message = input("Enter message to copy ")
+    my_list = []
+    print(f"list before {my_list}")
+    add_three_copies(my_list,message)
+    print(f"list after {my_list}")
+
+if __name__ == '__main__':
+    main()
+
