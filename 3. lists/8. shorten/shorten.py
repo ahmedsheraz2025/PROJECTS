12c6d19891a5a6082b8e66a3ade087c60de14fa1
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 18:27:41 +0500

    PIAIC python learning projects

--- 3. lists/8. shorten/shorten.py
+++ 3. lists/8. shorten/shorten.py
@@ -1,1 +1,22 @@
- 
+
+
+MAX_LENGTH: int = 4
+def shorten(lst):
+    while len(lst) > MAX_LENGTH:
+        i = lst.pop()
+        print(i)
+
+def get_lst():
+    lst = []
+    elem: str = input("Please enter element or press enter to stop: ")
+    while  elem != "":
+        lst.append(elem)
+        elem: str = input("Please enter element or press enter to stop: ")
+    return lst
+
+def main():
+    lst = get_lst()
+    shorten(lst)
+    
+if __name__ == '__main__':
+    main()
