5b20fdf6c517dddc9704de0a319fd65be424c4cb
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 17:38:47 +0500

    PIAIC python learning projects

--- 3. lists/6. get_last_element/get_last_element.py
+++ 3. lists/6. get_last_element/get_last_element.py
@@ -1,1 +1,20 @@
  
+def get_last_element(lst):
+    print(lst[-1])
+
+
+def get_lst():
+    elem: str = input("Please enter the element or press enter to stop: ")
+    lst = []
+    while elem != "":
+        lst.append(elem)
+        elem: str = input("Please enter the element or press enter to stop: ")
+    return lst
+
+
+def main():
+    lst = get_lst()
+    get_last_element(lst)
+    
+if __name__ == '__main__':
+    main()
