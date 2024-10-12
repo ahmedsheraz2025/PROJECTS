9f50512310e5d6e09c176305746863993d38299c
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-12 17:26:31 +0500

    PIAIC python learning project

--- 3. lists/5. get_first_element/get_first_element.py
+++ 3. lists/5. get_first_element/get_first_element.py
@@ -1,1 +1,20 @@
  
+ 
+ 
+def get_first_element(lst):
+    print(lst[0])
+ 
+def get_lst():
+    elem: str = input("Please enter element of the list or press enter to stop: ")
+    lst = []
+    while elem != "":
+        lst.append(elem)
+        elem: str = input("Please enter element of the list or press enter to stop: ")
+    return lst
+
+def main():
+    lst = get_lst()
+    get_first_element(lst)
+    
+if __name__ == '__main__':
+    main()
