525e964b00e4024c69aaefaac7b61596b320e660
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-15 12:49:46 +0500

    PIAIC python learning projects

--- 4. if_statement/3. leap_year/leap_year.py
+++ 4. if_statement/3. leap_year/leap_year.py
@@ -1,1 +1,15 @@
  
+# # Write a program that reads a year from the user and tells whether
+# # a given year is a leap year or not:
+
+def main():
+    user_year: int = int(input("Enter a year I'll tell you if it is leap or not: "))
+    if user_year % 4 == 0:
+        print(f"This is leap year {user_year}")
+    else:
+        print("This is not a leap year.")
+    
+    
+if __name__ == '__main__':
+    main()
+
