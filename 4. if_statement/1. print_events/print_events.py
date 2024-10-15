e49cef31392695fe007f23d4a0768abed2d7f829
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-15 12:22:31 +0500

    PIAIC python learning projects

---
+++ 4. if_statement/1. print_events/print_events.py
@@ -0,0 +1,16 @@
+ 
+# # Write a program that prints the first 20 even numbers. There are several correct
+# # approaches, but they all use a loop of some sort. Do no write twenty print statements:
+
+
+
+def main():
+    for num in range(20):# /40
+        # if num % 2 == 0:
+        #     print(num)
+        print(num * 2)
+    
+        
+    
+if __name__ == '__main__':
+    main()
