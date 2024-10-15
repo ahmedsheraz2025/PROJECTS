96a592d58a27461b34cbd47e38171f7df9d30806
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-15 12:55:27 +0500

    PIAIC python learning projects

--- 4. if_statement/4. tall_enough_to_ride/tall_enough_to_ride.py
+++ 4. if_statement/4. tall_enough_to_ride/tall_enough_to_ride.py
@@ -1,1 +1,15 @@
- 
+
+# # Write a program which asks the user how tall they are and prints
+# # whether or not they're taller than a pre-specified minimum height:
+
+
+def main():
+    user_input: int = int(input("Enter you age: "))
+    if user_input >= 10:
+        print("You're tall enough to ride.")
+    else:
+        print("You're not tall enough to ride, Maybe next year.")
+    
+    
+if __name__ == '__main__':
+    main()
