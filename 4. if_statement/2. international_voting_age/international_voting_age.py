8492a2694a3d507e6d25d69a2f6cd7ca6b7ce63a
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-15 12:44:34 +0500

    PIAIC python learning projects

--- 4. if_statement/2. international_voting_age/international_voting_age.py
+++ 4. if_statement/2. international_voting_age/international_voting_age.py
@@ -1,1 +1,17 @@
  
+# # Write a program which asks a user for their age and lets them know if they
+# # can or can't vote in the following three fictional countries.
+
+ 
+ 
+def main():
+    user_age: int = int(input("How old are you: "))
+    if user_age >= 48:
+        print("You can vote in Mayengua where the voting age is 48.")
+    elif user_age >= 25:
+        print("You can vote in Stanlauwhere the voting age is 25.")
+    elif user_age >= 16:
+        print("You can vote in Peturksbouipo where the votingage is 16.")
+    
+if __name__ == '__main__':
+    main()
