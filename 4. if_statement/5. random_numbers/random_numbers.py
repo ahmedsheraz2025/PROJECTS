88b33a82dfdfdd3297da03fb6945f89f984486d5
ahmedsheraz2025 <sherazjanjua99@gmail.com>
2024-10-15 13:01:28 +0500

    PIAIC python learning projects

--- 4. if_statement/5. random_numbers/random_numbers.py
+++ 4. if_statement/5. random_numbers/random_numbers.py
@@ -1,1 +1,26 @@
- 
+
+# # Print 10 random numbers in the range 1 to 100:
+
+
+import random
+
+def ran_num():
+    num = random.randint(1,100)
+    print(num)
+
+def main():
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    ran_num()
+    
+    
+if __name__ == '__main__':
+    main()
+
